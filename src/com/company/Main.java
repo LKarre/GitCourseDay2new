6bce6394da7b4434122dba3ea4dbe86b13725b0f
LKarre <liselotte.karre@hm.com>
2021-02-05 11:28:28 +0100

    New feature in branch

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,11 +6,16 @@ public class Main {
         int num = 26;
         int increment = increment(num);
         int newNumber = increment(increment);
+        int decrement = decrement(increment);
 
         System.out.println(increment);
         System.out.println(newNumber);
+        System.out.println(decrement);
     }
     public static int increment (int number) {
         return ++number;
+    }
+    public static int decrement (int number){
+        return --number;
     }
 }
